730d13947e7186e7f251d625f15d6b7e7f2a18d8
Your Name <you@example.com>
2017-05-06 15:21:57 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Models.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Logn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Logn(LogonViewModel padeDate)
+        {
+            if (padeDate.Account == "skill" &&
+                padeDate.Password == "tree")
+            {
+                padeDate.Message =
+                    $"您使用{padeDate.Account}登入成功。";
+            }
+            else
+            {
+                padeDate.Message =
+                 $"您使用{padeDate.Account}登入失敗。";
+            }
+           
+            return View(padeDate);
+        }
+    }
+    
+}
---
+++ WebApplication1/Models/ViewModels/LogonViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class LogonViewModel
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string Message { get; set; }
+       
+    }
+}
